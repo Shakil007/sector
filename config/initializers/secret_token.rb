2654701dbc29b170c164c99217eb4f6ba1caf334
Shakil007 <mehedi_nsu07@hotmail.com>
2014-09-01 05:49:53 -0400

    pg gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sector::Application.config.secret_token = 'f3faf55ef74d3c828720c49f7de78ec4b36fc5696771f2baccb8ba9567f6872a15baa8b8e287aeca8b6deaa5b8d4cdc822679414d91c750577ba0a5ab44dbbe8'
